41dc2af6caad32bd5516a5985b6be7ee973d32ea
lovisavh <lvh@kth.se>
2015-05-17 23:21:45 +0200

    Created loop in Keyboard

--- src/KeyboardGUI.java
+++ src/KeyboardGUI.java
@@ -43,31 +43,61 @@ public class KeyboardGUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 688, 441);
+		frame.setBounds(100, 100, 1500, 441);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
-		JButton btnNewButton = new JButton("c1");
-		btnNewButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
+
+
+		/**
+		 * Creating white keys
+		 */
+		int xw = 0;
+		int xb = 45;
+		int blackToDraw = 2;
+
+		for(int i = 0; i < 25; i++) {
+			JButton buttonb1 = new JButton("b1");
+			buttonb1.setBackground(Color.WHITE);
+			buttonb1.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+				}
+			});
+			buttonb1.setBounds(xw, 159, 60, 250);
+			frame.getContentPane().add(buttonb1);
+			xw += 60;
+		}
+
+		/**
+		 * Creating black keys
+		 */
+		for (int j = 0; j < blackToDraw; j++) {
+
+
+			JButton btnNewButton = new JButton("c1");
+			btnNewButton.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent arg0) {
+				}
+			});
+			btnNewButton.setBackground(Color.BLACK);
+			btnNewButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+				}
+			});
+			btnNewButton.setBounds(xb, 60, 30, 100);
+			frame.getContentPane().add(btnNewButton);
+			xb += 60;
+
+			if (j == 3){
+				blackToDraw = 2;
+				j = 0;
+				xb += 60;
+			} else if (j == 2) {
+				blackToDraw = 3;
+				j = 0;
+				xb += 60;
 			}
-		});
-		btnNewButton.setBackground(Color.BLACK);
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton.setBounds(235, 60, 45, 101);
-		frame.getContentPane().add(btnNewButton);
-		
-		JButton buttonb1= new JButton("b1");
-		buttonb1.setBackground(Color.WHITE);
-		buttonb1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		buttonb1.setBounds(104, 159, 66, 122);
-		frame.getContentPane().add(buttonb1);
-	}
-}
+
+		}
+
+	}}
